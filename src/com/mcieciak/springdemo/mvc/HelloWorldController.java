f0a04b91b9c865819ee5bed05900cbb5ac9518f5
Mateusz Cieciak <mc424i@gmail.com>
2016-10-20 13:54:21 +0200

    adding data to the Spring model, using HTTPServletRequest

--- src/com/mcieciak/springdemo/mvc/HelloWorldController.java
+++ src/com/mcieciak/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.mcieciak.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -13,11 +16,30 @@ public class HelloWorldController {
 
     }
 
-
     //    controller method to process the html form
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
     }
+
+    //    controller method to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        //      read the req parameter from html form
+        String theName = request.getParameter("studentName");
+
+        //      convert data to allCaps
+        theName = theName.toUpperCase();
+
+        //      create the message
+        String result = "yo! " + theName;
+
+        //      add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
 
 }
--- web/WEB-INF/view/helloworld.jsp
+++ web/WEB-INF/view/helloworld.jsp
@@ -5,8 +5,10 @@
 Hello world of Spring!
 
 <br><br>
-
 Student name: ${param.studentName}
+
+<br><br>
+The message: ${message}
 
 </body>
 
